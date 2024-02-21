5f1997edbae12aa4cf4f2a65913c1601a9d078af
Siddhant Verma <karanspd1289@gmail.com>
2024-02-21 12:38:07 +0530

    created event handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,36 @@ import { Component } from '@angular/core';
               <h3>{{title}}</h3>
               <img [src]="image" height=175 width=300>
               <input type="text" name=nameText [(ngModel)]="textData"/>
-              <p>{{textData}}</p>` 
+              <p>{{textData}}</p>
+              <input type=text (keyup)=onKeyUp($event)>
+              <p>{{myText}}</p>
+              <button (click)="changeImage()">Change Image</button>
+              <img [src]="currentImageUrl" alt="Image" height=175 width=300>` 
 })
 export class AppComponent {
   title = 'Default Component';
   image = "./assets/gun.jpg";
   textData = "Siddhant";
+  myText = "Old text";
+  imageUrls: string[] = [
+    './assets/man.jpg',
+    './assets/catcav.jpg',
+    './assets/gun.jpg',
+    './assets/cat.jpg',
+    './assets/kid.jpg',
+    './assets/dog.jpg'
+  ];
+
+  currentIndex: number = 0;
+  currentImageUrl: string = this.imageUrls[0];
+
+  changeImage() {
+    // Increment the counter and cycle through the image URLs
+    this.currentIndex = (this.currentIndex + 1) % this.imageUrls.length;
+    this.currentImageUrl = this.imageUrls[this.currentIndex];
+  }
+
+  onKeyUp(event: any){
+    this.myText = event.target.value;
+  }
 }
