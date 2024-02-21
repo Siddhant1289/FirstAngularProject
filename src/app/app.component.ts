4bd4ce6248d08411e33f92eabac472816981f577
Siddhant Verma <karanspd1289@gmail.com>
2024-02-21 11:28:50 +0530

    Started With App Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  /*templateUrl: './app.component.html',
+  styleUrl: './app.component.css'*/
+  template:   `<h1>Angular Application Demo</h1>
+              <h3>{{title}}</h3>
+              <img [src]="image" height=175 width=300>
+              <input type="text" name=nameText [(ngModel)]="textData"/>
+              <p>{{textData}}</p>` 
 })
 export class AppComponent {
-  title = 'FirstAngularProject';
+  title = 'Default Component';
+  image = "./assets/gun.jpg";
+  textData = "Siddhant";
 }
